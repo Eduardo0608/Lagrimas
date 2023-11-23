336c3d04b779d8f1a81dbedeb044eda624df0e77
RamonPeixe <r-amoncp@hotmail.com>
2023-11-22 21:44:58 -0300

    test: creating shouldOpenTearsRegisterPage test

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,27 +1,31 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainTest {
 
     private WebDriver driver;
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
         WebDriverManager.firefoxdriver().setup();
         driver = new FirefoxDriver();
         driver.get("http://localhost:3000/");
     }
 
     @AfterEach
-    void tearDown(){
+    void tearDown() {
         driver.quit();
     }
 
     @Test
     @DisplayName("Should open and close google")
-    public void shouldOpenAndCloseGoogle() throws InterruptedException{
+    public void shouldOpenAndCloseGoogle() throws InterruptedException {
         setUp();
         driver.get("https://www.google.com/");
         Thread.sleep(1000);
@@ -30,9 +34,25 @@ public class MainTest {
 
     @Test
     @DisplayName("Should open and close Lagrimas project")
-    public void shouldOpenAndCloseLagrimasProject() throws InterruptedException{
+    public void shouldOpenAndCloseLagrimasProject() throws InterruptedException {
         setUp();
         Thread.sleep(1000);
+        tearDown();
+    }
+
+    @Test
+    @DisplayName("Should open tear's register page")
+    public void shouldOpenTearsRegisterPage() throws InterruptedException {
+        setUp();
+        WebElement registerPath = driver.findElement(By.xpath("//a[@href='/cadastrar']"));
+        registerPath.click();
+
+        Thread.sleep(1000);
+
+        String novaURL = driver.getCurrentUrl();
+
+        assertEquals("http://localhost:3000/cadastrar", novaURL);
+
         tearDown();
     }
 }
