41608a00fe5f7d4f1518c2e37179397dcde3ec75
RamonPeixe <r-amoncp@hotmail.com>
2023-11-23 21:35:38 -0300

    test: creating shouldNavigateToEditPageWhenClickingEditButton test

--- src/test/java/LagrimasTest.java
+++ src/test/java/LagrimasTest.java
@@ -271,4 +271,13 @@ public class LagrimasTest {
         assertEquals("http://localhost:3000/", currentUrl);
     }
 
+    @Test
+    @DisplayName("Should navigate to Edit page when clicking edit button")
+    void shouldNavigateToEditPageWhenClickingEditButton() {
+        driver.get("http://localhost:3000/");
+        WebElement editButton = driver.findElement(By.xpath("(//div[@class='lagrima-card']//a)[1]"));
+        editButton.click();
+        String currentUrl = driver.getCurrentUrl();
+        assertEquals("http://localhost:3000/lagrima?id=2", currentUrl);
+    }
 }
