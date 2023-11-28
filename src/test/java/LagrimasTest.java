c0923154a529198d2ddb79ffb81b5f5660a0038c
RamonPeixe <r-amoncp@hotmail.com>
2023-11-28 18:42:07 -0300

    test: creating shouldNavigateToEditPageAndDoNotFillInAnyFields test

--- src/test/java/LagrimasTest.java
+++ src/test/java/LagrimasTest.java
@@ -347,4 +347,36 @@ public class LagrimasTest {
         assertEquals("", categoryValue);
         assertEquals("", authorValue);
     }
+
+    @Test
+    @DisplayName("Should navigate to edit page and do not fill in any fields")
+    void shouldNavigateToEditPageAndDoNotFillInAnyFields() throws InterruptedException {
+        driver.get("http://localhost:3000/");
+        WebElement editButton = driver.findElement(By.xpath("(//div[@class='lagrima-card']//a)[1]"));
+        editButton.click();
+        WebElement nameInput = driver.findElement(By.id("nome"));
+        nameInput.clear();
+        WebElement categoryInput = driver.findElement(By.id("categoria"));
+        categoryInput.clear();
+        WebElement authorInput = driver.findElement(By.id("autor"));
+        authorInput.clear();
+        Thread.sleep(1000);
+        WebElement editButton2 = driver.findElement(By.id("editar-button"));
+        editButton2.click();
+        Thread.sleep(1000);
+        Alert alert = driver.switchTo().alert();
+        alert.accept();
+        Thread.sleep(1000);
+        Alert alert2 = driver.switchTo().alert();
+        alert2.accept();
+        WebElement nameInputReload = driver.findElement(By.id("nome"));
+        WebElement categoryInputReload = driver.findElement(By.id("categoria"));
+        WebElement authorInputReload = driver.findElement(By.id("autor"));
+        String nameValue = nameInputReload.getAttribute("value");
+        String categoryValue = categoryInputReload.getAttribute("value");
+        String authorValue = authorInputReload.getAttribute("value");
+        assertEquals("", nameValue);
+        assertEquals("", categoryValue);
+        assertEquals("", authorValue);
+    }
 }
